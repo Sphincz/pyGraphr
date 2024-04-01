6a95a23c946c3bdcf7e5835de3797dd521a06e23
António Raimundo <aslro@iscte-iul.pt>
2024-04-01 20:01:38 +0100

    pyGraphr - v1.5-BETA ~ Minor fixes

--- src/utils/hierarchy_network.py
+++ src/utils/hierarchy_network.py
@@ -2,7 +2,7 @@ import networkx as nx
 import random
 
 
-def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5):
+def hierarchy_pos(g, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5):
     """
     From Joel's answer at https://stackoverflow.com/a/29597209/2966723.
     Licensed under Creative Commons Attribution-Share Alike
@@ -10,7 +10,7 @@ def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5)
     If the graph is a tree this will return the positions to plot this in a
     hierarchical layout.
 
-    G: the graph (must be a tree)
+    g: the graph (must be a tree)
 
     root: the root node of current branch
     - if the tree is directed and this is not given,
@@ -28,32 +28,32 @@ def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5)
 
     xcenter: horizontal location of root
     """
-    # if not nx.is_tree(G):
+    # if not nx.is_tree(g):
     #    raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
 
     if root is None:
-        if isinstance(G, nx.DiGraph):
-            root = next(iter(nx.topological_sort(G)))  # allows back compatibility with nx version 1.11
+        if isinstance(g, nx.DiGraph):
+            root = next(iter(nx.topological_sort(g)))  # allows back compatibility with nx version 1.11
         else:
-            root = random.choice(list(G.nodes))
+            root = random.choice(list(g.nodes))
 
-    def _hierarchy_pos(G, root, width=1., vert_gap=0.0, vert_loc=0.0, xcenter=0.0, pos=None, parent=None):
+    def _hierarchy_pos(g, root, width=1., vert_gap=0.0, vert_loc=0.0, xcenter=0.0, pos=None, parent=None):
 
         if pos is None:
             pos = {root: (xcenter, vert_loc)}
         else:
             pos[root] = (xcenter, vert_loc)
-        children = list(G.neighbors(root))
-        if not isinstance(G, nx.DiGraph) and parent is not None:
+        children = list(g.neighbors(root))
+        if not isinstance(g, nx.DiGraph) and parent is not None:
             children.remove(parent)
         if len(children) != 0:
             dx = width / len(children)
             nextx = xcenter - width / 2 - dx / 2
             for child in children:
                 nextx += dx
-                pos = _hierarchy_pos(G, child, width=dx, vert_gap=vert_gap,
+                pos = _hierarchy_pos(g, child, width=dx, vert_gap=vert_gap,
                                      vert_loc=vert_loc - vert_gap, xcenter=nextx,
                                      pos=pos, parent=root)
         return pos
 
-    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+    return _hierarchy_pos(g, root, width, vert_gap, vert_loc, xcenter)
